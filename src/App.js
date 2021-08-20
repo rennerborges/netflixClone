bdabd90d44de352a722f04cc86b36c09c46e20a9
Renner Borges <rennerferreira23@gmail.com>
2021-08-20 15:39:15 -0300

    feat: listando listas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      const list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, [])
+
   return (
-    <div>
-      Olá mundo!
+    <div className="page">
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div key={key}>
+            {item.title}
+          </div>
+        ))}
+      </section>
+
     </div>
   );
 }
