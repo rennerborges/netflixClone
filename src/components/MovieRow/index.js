679919438047f86b269198defa26029009d4e498
Renner Borges <rennerferreira23@gmail.com>
2021-08-21 15:34:02 -0300

    feat: init arrow list

--- src/components/MovieRow/index.js
+++ src/components/MovieRow/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './index.css';
 
+import {
+    NavigateBefore,
+    NavigateNext
+} from '@material-ui/icons';
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ title, items }) => {
     return (
         <div className="movieRow">
             <h2>{title}</h2>
+
+            <div className="movieRow--left">
+                <NavigateBefore style={{ fontSize: 50 }} />
+            </div>
+            <div className="movieRow--right">
+                <NavigateNext style={{ fontSize: 50 }} />
+            </div>
+
             <div className="movieRow--listarea">
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
